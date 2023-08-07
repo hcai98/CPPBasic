2b9fd4adcea9e531b56791565f63ac3f88a4c0de
billcai98 <!@#$Bc980923>
2023-08-06 21:44:06 -0700

    pointers

--- src/19_pointers.cpp
+++ src/19_pointers.cpp
@@ -3,5 +3,27 @@ using namespace std;
 
 int main()
 {
+    int age = 19;
+    double gpa = 2.7;
+    string name = "Mike";
 
+    // print out pointers
+    cout << "Address of gpa: " << &gpa << endl;
+    cout << "Address of age: " << &age << endl;
+    cout << "Address of name: " << &name << endl;
+
+    // create variables to keep track of the pointers
+    int *pAge = &age;
+    double *pGpa = &gpa;
+    string *pName = &name;
+
+    // print out pointers
+    cout << "Address of gpa: " << pGpa << endl;
+    cout << "Address of age: " << pAge << endl;
+    cout << "Address of name: " << pName << endl;
+
+    // dereferencing pointers
+    cout << "Value at pAge: " << *pAge << endl;
+
+    return 0;
 }
